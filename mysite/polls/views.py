65eb3e929ea042fb721d9f10bffa195aa6da7d58
jswwWorks <jsww.works@gmail.com>
2024-02-05 19:04:55 -0500

    Added detail form and updated views and paths of polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+# For when we needed Http404
+# from django.http import Http404, HttpResponse
 
 from .models import Question
 
@@ -31,12 +34,17 @@ def index(request):
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
